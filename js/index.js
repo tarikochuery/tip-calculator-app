cbd36e316810e1d91d3cf2f46a2325e663fbdd0a
tariko <ttarikechuery@gmail.com>
2022-08-07 12:16:02 -0300

    All features implemented

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const tipPercentCustomInput = document.getElementById('tip-percent-input')
 const totalPeopleInput = document.getElementById('number-costumer')
 const tipAmountStrong = document.getElementById('tip-amount')
 const totalStrong = document.getElementById('total')
+const resetButton = document.getElementById('reset-button')
 
 let tipCalculatorState = {
   totalBill: 0,
@@ -15,6 +16,8 @@ let tipCalculatorState = {
   tipForEach: 0,
   totalForEach: 0
 }
+
+const INPUT_OUTLINE_STYLE = '1px solid var(--strong-cyan)'
 
 const handleTotalBillChange = (e) => {
   tipCalculatorState.totalBill = Number(e.target.value)
@@ -52,14 +55,25 @@ const handleTipCustomChange = (e) => {
   calculateAndUpdateScreen()
 }
 
+const handleResetClick = () => {
+  tipCalculatorState.people = 0
+  tipCalculatorState.percentSelected = false
+  tipCalculatorState.tipForEach = 0
+  tipCalculatorState.tipPercent = 0
+  tipCalculatorState.totalBill = 0
+  tipCalculatorState.totalForEach = 0
+  tipCalculatorState.totalTip = 0
 
-totalBillInput.addEventListener('keyup', handleTotalBillChange)
-totalPeopleInput.addEventListener('keyup', handlePeopleChange)
-tipPercentCustomInput.addEventListener('keyup', handleTipCustomChange)
+  tipPercentButtonsArray.forEach(btn => {
+    btn.classList.remove('selected')
+  })
 
-tipPercentButtonsArray.forEach(button => {
-  button.addEventListener('click', handleTipButtonClick)
-})
+  totalBillInput.value = ''
+  totalPeopleInput.value = ''
+  tipPercentCustomInput.value = ''
+  totalStrong.innerText = '0.00'
+  tipAmountStrong.innerText = '0.00'
+}
 
 const calculateTotalTip = () => {
   const { tipPercent, totalBill } = tipCalculatorState
@@ -84,7 +98,7 @@ const calculateTotalForEach = () => {
 }
 
 const updateScreen = (element, value) => {
-  element.innerText = value
+  element.innerText = `$${value}`
 }
 
 
@@ -98,3 +112,25 @@ const calculateAndUpdateScreen = () => {
   }
   return
 }
+
+const handleFocusInInput = (e) => {
+  e.target.parentElement.style.outline = INPUT_OUTLINE_STYLE
+}
+
+const handleFocusOutInput = (e) => {
+  e.target.parentElement.style.outline = 'none'
+}
+
+totalBillInput.addEventListener('keyup', handleTotalBillChange)
+totalBillInput.addEventListener('focusin', handleFocusInInput)
+totalBillInput.addEventListener('focusout', handleFocusOutInput)
+totalPeopleInput.addEventListener('keyup', handlePeopleChange)
+totalPeopleInput.addEventListener('focusin', handleFocusInInput)
+totalPeopleInput.addEventListener('focusout', handleFocusOutInput)
+tipPercentCustomInput.addEventListener('keyup', handleTipCustomChange)
+resetButton.addEventListener('click', handleResetClick)
+
+tipPercentButtonsArray.forEach(button => {
+  button.addEventListener('click', handleTipButtonClick)
+})
+
